1ff1d4dbf191b1f2201caa09cdd1a0de8b4c01b7
olafvanv <olafvanv@gmail.com>
2021-09-15 19:52:31 +0200

    fix: use default export instead of named export

--- src/ClusterFeatureLayer.js
+++ src/ClusterFeatureLayer.js
@@ -1,6 +1,8 @@
 import { setOptions, GeoJSON, markerClusterGroup } from 'leaflet';
 import { FeatureManager } from 'esri-leaflet';
-export { version as VERSION } from '../package.json';
+import packageInfo from '../package.json';
+var version = packageInfo.version;
+export { version as VERSION };
 
 export var FeatureLayer = FeatureManager.extend({
 
@@ -172,7 +174,7 @@ export var FeatureLayer = FeatureManager.extend({
   }
 });
 
-export function featureLayer (options) {
+export function featureLayer(options) {
   return new FeatureLayer(options);
 }
 
