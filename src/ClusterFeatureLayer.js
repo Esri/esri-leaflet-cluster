4f592ca76fa6bd35022f9596dcf6df92b598da31
Jakub Mank <jakubmank@gmail.com>
2021-11-16 05:59:08 -0500

    fix: Use default export from package instead of named export version.
    Using named exports from JSON modules is not supported by the new specification in Webpack v5

--- src/ClusterFeatureLayer.js
+++ src/ClusterFeatureLayer.js
@@ -1,6 +1,7 @@
 import { setOptions, GeoJSON, markerClusterGroup } from 'leaflet';
 import { FeatureManager } from 'esri-leaflet';
-export { version as VERSION } from '../package.json';
+import packageInfo from '../package.json';
+export var VERSION = packageInfo.version;
 
 export var FeatureLayer = FeatureManager.extend({
 
